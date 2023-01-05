56eaa2d419775427659368c96368d572adcb6d91
Helio H. Mocumbi <heliomucumbi@gmail.com>
2023-01-04 22:35:03 -0800

    Update best_answer_id on question table when an answer removed using Eloquent event

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -35,11 +35,21 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id === $answer->id){
+                $question->best_answer_id = null;
+                $question->save();
+            }
         });
     }
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
